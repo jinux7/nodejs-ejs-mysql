92872f2f4d9676df4c0fd0f4e9cd9d78817af075
xuj <xuj@bjyn.com>
2018-03-20 11:34:50 +0800

    添加less功能。

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nodejs-ejs-mysql
 nodejs,ejs模版，使用mysql数据库
+
+### 可以使用less
+/src/asset/less 文件夹下编写.less文件，使用了 express-less 包来编译less，直接在页面访问/static/css/style.css即可。
--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-var multer = require('multer'); 
+const multer = require('multer'); 
 const consolidate = require('consolidate');
 const route = require('./src/route/index');
-
+expressLess = require('express-less');
 var app = express();
+//设置静态文件夹//处理less
+app.use('/static/css', expressLess(__dirname + '/src/asset/less'));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-//app.use(multer()); // for parsing multipart/form-data
-//设置静态文件夹
 app.use('/static', express.static('public'));
 //设置模板
 app.set('view engine','html');
 app.set('views','./views');
 app.engine('html',consolidate.ejs);
-
 //加载路由
 app.use('/',route);
 //创建服务器
